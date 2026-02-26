0ecb5db8cf88ed1ab4736ada9aa299c0f103fdb2
louis mathew <louiskumarmathew@proton.me>
2026-02-26 04:30:34 -0500

    corrected rename erros 'simple' -> 'erasebg'

--- erasebg/urls.py
+++ erasebg/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for simple project.
+URL configuration for erasebg project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
